e3dad4db38cb907cfcc93374b74ce0108e120971
Anna Dai <adai@student.ethz.ch>
2019-06-04 11:19:35 +0100

    add raycast for occluded voxels

--- se_denseslam/src/bfusion/rendering_impl.hpp
+++ se_denseslam/src/bfusion/rendering_impl.hpp
@@ -31,6 +31,7 @@
 
 #include <se/utils/math_utils.h>
 #include <type_traits>
+#include <unordered_set>
 
 inline Eigen::Vector4f raycast(const Volume<OFusion>& volume,
                                const Eigen::Vector3f& origin,
@@ -42,20 +43,23 @@ inline Eigen::Vector4f raycast(const Volume<OFusion>& volume,
                                const float) {
 
   auto select_occupancy = [](const auto& val){ return val.x; };
+  // march from camera away
   if (tnear < tfar) {
-    float t = tnear;
+    float t = tnear; // closer bound to camera
     float stepsize = step;
+    // occupancy prob in log2
     float f_t = volume.interp(origin + direction * t, select_occupancy);
     float f_tt = 0;
-
-    // if we are not already in it
+    // check if current pos is free
     if (f_t <= SURF_BOUNDARY) {
       for (; t < tfar; t += stepsize) {
         const Eigen::Vector3f pos =  origin + direction * t;
         Volume<OFusion>::value_type data = volume.get(pos);
+        // check if the voxel has been updated before
         if (data.x > -100.f && data.y > 0.f) {
           f_tt = volume.interp(origin + direction * t, select_occupancy);
         }
+        // log2(p_occ)>0 , current pos is occupied
         if (f_tt > SURF_BOUNDARY)
           break;
         f_t = f_tt;
@@ -64,6 +68,7 @@ inline Eigen::Vector4f raycast(const Volume<OFusion>& volume,
         // got it, calculate accurate intersection
         t = t - stepsize * (f_tt - SURF_BOUNDARY) / (f_tt - f_t);
         Eigen::Vector4f res = (origin + direction * t).homogeneous();
+        // normalized distance between camera and surface
         res.w() = t;
         return res;
       }
@@ -72,3 +77,39 @@ inline Eigen::Vector4f raycast(const Volume<OFusion>& volume,
   return Eigen::Vector4f::Constant(0);
 }
 
+inline bool raycastOcclusionVoxel(const Volume<OFusion>& volume,
+                                  const Eigen::Vector3f& origin,
+                                  const Eigen::Vector3f& direction,
+                                  const float            tnear,
+                                  const float            tfar,
+                                  const float,
+                                  const float            step,
+                                  const float,
+                                  std::unordered_set<uint64_t> &occlusion_voxel,
+                                  const float inverseVoxelSize) {
+
+  if (tnear < tfar) {
+    // t = starting distance after surface
+    float t = tnear+step;
+    float stepsize = step;
+
+    //march along the ray from surface away
+    for (; t < tfar; t += stepsize) {
+      const Eigen::Vector3f pos =  origin + direction * t;
+      // get info from map
+      Volume<OFusion>::value_type data = volume.get(pos);
+      // using log odds prob as -100 is a threshold for free space
+      // if valid add morton code to occluded voxel set
+      if (data.x > -100.f && data.y > 0.f) {
+        Eigen::Vector4f occl = (origin + direction * t).homogeneous();
+//          std::cout << "occlusion "<< res << std::endl;
+        Eigen::Vector3i  occl_scaled = (inverseVoxelSize * occl.head<3>()).cast<int>();
+        occlusion_voxel.insert(compute_morton(occl_scaled.x(), occl_scaled.y(), occl_scaled.z()));
+
+      }
+
+    }
+
+  }
+  return true;
+}
