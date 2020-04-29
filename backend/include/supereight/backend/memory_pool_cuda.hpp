c1b69ab9ac80fc31f0957e5914a62e8b6a2dc519
Joe Kaushal <jk1616@ic.ac.uk>
2020-04-29 15:44:26 +0100

    Implement CUDA memory pool dynamic resize

--- backend/include/supereight/backend/memory_pool_cuda.hpp
+++ backend/include/supereight/backend/memory_pool_cuda.hpp
@@ -11,13 +11,10 @@ template<typename T>
 class MemoryPoolCUDA {
 public:
     SE_DEVICE_FUNC
-    MemoryPoolCUDA() {
-        safeCall(cudaMallocManaged(&buffer_, capacity_ * sizeof(T)));
-    };
+    MemoryPoolCUDA(){};
 
     SE_DEVICE_FUNC
-    MemoryPoolCUDA(const MemoryPoolCUDA& other)
-        : used_{other.used_}, buffer_{other.buffer_} {}
+    MemoryPoolCUDA(const MemoryPoolCUDA&) = default;
 
     SE_DEVICE_FUNC
     ~MemoryPoolCUDA() {}
@@ -34,16 +31,57 @@ public:
         return (*this)[idx];
     }
 
-    void reserve(int) {}
+    void reserve(int n) {
+        if (n <= capacity_) return;
+        int num_pages = (n - capacity() + page_size_ - 1) / page_size_;
+
+        if (page_table_capacity_ < page_table_used_ + num_pages) {
+            int new_capacity = (page_table_used_ + num_pages) * 1.5;
+
+            T** new_table;
+            safeCall(cudaMallocManaged(&new_table, new_capacity * sizeof(T*)));
+
+            if (page_table_ != nullptr) {
+                safeCall(cudaMemcpy(new_table, page_table_,
+                    page_table_capacity_ * sizeof(T*),
+                    cudaMemcpyDeviceToDevice));
+                safeCall(cudaFree(page_table_));
+            }
+
+            page_table_          = new_table;
+            page_table_capacity_ = new_capacity;
+        }
+
+        for (int i = 0; i < num_pages; ++i) {
+            T* new_page;
+            safeCall(cudaMallocManaged(&new_page, page_size_ * sizeof(T)));
+
+            safeCall(cudaMemcpy(page_table_ + page_table_used_, &new_page,
+                sizeof(T*), cudaMemcpyHostToDevice));
+            page_table_used_++;
+
+            capacity_ += page_size_;
+        }
+    }
 
     SE_DEVICE_FUNC
-    T* operator[](int i) const { return buffer_ + i; }
+    T* operator[](int i) const {
+        int page   = i / page_size_;
+        int offset = i % page_size_;
+
+        return page_table_[page] + offset;
+    }
 
 private:
-    constexpr static int capacity_ = 36728;
-    int used_                      = 0;
+    static constexpr int page_size_ = 32 * 4096 / sizeof(T);
 
-    T* buffer_;
+    int capacity_ = 0;
+    int used_     = 0;
+
+    T** page_table_ = nullptr;
+
+    int page_table_used_     = 0;
+    int page_table_capacity_ = 0;
 };
 
 } // namespace se
